b6621b5ac69824ca713e22fe9044f0be2f783950
Christopher Quiles <quileswest@gamil.com>
2022-06-06 10:36:00 -0700

    feat: adding files for aws/docker-swarm

---
+++ security_groups.tf
@@ -0,0 +1,43 @@
+
+resource "aws_security_group" "default" {
+  name = "sgswarmcluster-cquiles"
+
+  # Allow all inbound
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    # SSH Traffic
+  ingress {
+    description = "SSH"
+    from_port   = 0 
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+#   egress {
+#     from_port   = 22
+#     to_port     = 0  # SSH client port is not a fixed port
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+  # Enable ICMP
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
